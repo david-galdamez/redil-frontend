e34055f774f73b697ffd513136c4469c7680e0c4
David Morales <37608584+davidamorales22@users.noreply.github.com>
2026-05-26 20:42:17 -0600

    chore: cloudflare setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,15 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
-
+import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import react from "@astrojs/react";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: cloudflare(),
   integrations: [react()],
   vite: {
-    server: {
-      proxy: {
-        '/api': {
-          target: 'https://comu-redil-api-development.up.railway.app',
-          changeOrigin: true,
-          secure: true,
-        }
-      }
-    },
     plugins: [tailwindcss()]
   }
 });
