b99a7ae53aa9258952c81de3b8bc956c443736cd
David Morales <37608584+davidamorales22@users.noreply.github.com>
2026-05-28 22:04:53 -0600

    chore: add missing include

--- src/hooks/useRedilRegisterForm.ts
+++ src/hooks/useRedilRegisterForm.ts
@@ -33,6 +33,7 @@ export function useRedilRegisterForm(apiUrl: string, redilCode: string) {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(form),
+                credentials: "include",
             });
 
             const data = (await res.json()) as ApiResponse<number>;
