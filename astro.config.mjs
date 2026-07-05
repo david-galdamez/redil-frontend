971c3683855ea75e4cd77a2f06edab0840da7e49
David Galdamez <david.galdamez2014@gmail.com>
2026-07-05 09:06:00 -0600

    feat: added netlify support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import cloudflare from '@astrojs/cloudflare';
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
+  adapter: netlify(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
